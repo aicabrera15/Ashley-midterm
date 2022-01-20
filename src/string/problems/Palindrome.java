a605eafb8692884cafa10d38cc6229908304403f
aicabreraa15 <aicabreraa15@gmail.com>
2022-01-19 22:59:42 -0500

    design

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) {
         /*
@@ -7,5 +9,22 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+        String str1, str2="";       //initialize strings
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Type a word to see if is palindrome or not palindrome: ");
+        str1= sc.nextLine();
+        int i, n= str1.length();
+        for(i=n-1; i>=0; i--)
+        {
+            str2= str2+ str1.charAt(i);      //reversing the string
+        }
+        if(str1.equalsIgnoreCase(str2))      //checking the strings
+        {
+            System.out.println("The String is palindrome");
+        }
+        else
+        {
+            System.out.println("The String is not palindrome");
+        }
     }
 }
